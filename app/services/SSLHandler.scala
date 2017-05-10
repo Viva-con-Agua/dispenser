7b05d69bac109cb7af78c720a73e884d09f30255
Dennis Kleber <dennis_kleber@mailbox.org>
2017-05-10 19:37:48 +0200

    add SSLHandler -- bug fix

--- app/services/SSLHandler.scala
+++ app/services/SSLHandler.scala
@@ -1,8 +1,10 @@
+package server
+
 import javax.net.ssl._
 import play.core.ApplicationProvider
 import play.server.api._
 
-class CustomSSLEngineProvider(appProvider: ApplicationProvider) extends SSLEngineProvider {
+class SSLHandler(appProvider: ApplicationProvider) extends SSLEngineProvider {
 
   override def createSSLEngine(): SSLEngine = {
     // change it to your custom implementation
